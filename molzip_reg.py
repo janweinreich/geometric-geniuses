8c96f441037c6a572dd2aabfefa7c679e41fb01b
janweinreich <janweinreich286@gmail.com>
2024-05-02 20:23:44 +0200

    Fix pdb.set_trace() statement in molzip_reg.py

--- molzip_reg.py
+++ molzip_reg.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    pdb.set_trace()
+
     converter = ZipFeaturizer()
     X_train = converter.bin_vectors(X_train)
     X_test = converter.bin_vectors(X_test)
