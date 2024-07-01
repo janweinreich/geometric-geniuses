b9a57d8e8176b8214b45fdde96f7c97772364914
janweinreich <janweinreich286@gmail.com>
2024-07-01 09:32:31 +0200

    ...

--- Feat2LLM/roberta_finetuning.py
+++ Feat2LLM/roberta_finetuning.py
@@ -118,8 +118,8 @@ if __name__ == "__main__":
     optimizer = AdamW(model.parameters(), lr=1e-6)
 
     # DataLoader setup
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
     # Training loop
     model.train()
